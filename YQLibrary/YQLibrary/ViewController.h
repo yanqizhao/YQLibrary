9dc0d8724b432d55345e6a479fed0a6f6792ece4
小蘑菇 <xiaomogu@xiaomogudeMac-mini.local>
2017-04-12 16:22:10 +0800

    init

---
+++ YQLibrary/YQLibrary/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YQLibrary
+//
+//  Created by 小蘑菇 on 2017/4/12.
+//  Copyright © 2017年 zhaoyanqi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ YQLibrary/YQLibrary/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  YQLibrary
+//
+//  Created by 小蘑菇 on 2017/4/12.
+//  Copyright © 2017年 zhaoyanqi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
